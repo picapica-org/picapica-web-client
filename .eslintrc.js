a2147e2703fdb4e7df362ce161d56e084d668153
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-09-10 12:57:53 +0200

    Added configs and misc fiels

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	globals: {
+		__PATH_PREFIX__: true,
+	},
+	parser: `@typescript-eslint/parser`,
+	extends: ["plugin:@typescript-eslint/recommended", "react-app", "plugin:prettier/recommended", "prettier"],
+	plugins: ["@typescript-eslint", "prettier"],
+	parserOptions: {
+		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+		sourceType: "module", // Allows for the use of imports
+	},
+	env: {
+		browser: true,
+		node: true,
+	},
+	rules: {
+		"@typescript-eslint/explicit-function-return-type": [
+			"error",
+			{
+				allowExpressions: true,
+			},
+		],
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"no-restricted-globals": "off",
+	},
+	ignorePatterns: ["*.css", "*.scss", "gulpfile.js", "publish/**", "src/lib/generated/**"],
+};
