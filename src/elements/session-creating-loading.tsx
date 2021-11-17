31c39e0243f784b29e3ba5537fddc94de74cac15
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-11-17 12:57:21 +0100

    Added creating screen

--- src/elements/session-creating-loading.tsx
+++ src/elements/session-creating-loading.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import { Link } from "gatsby";
 import { getLocalization, Locales, LocalizableProps, SimpleString } from "../lib/localization";
-import { Loading } from "../lib/use-session";
-import "./session-loading.scss";
+import { Creating, Loading } from "../lib/use-session";
+import "./session-creating-loading.scss";
+
+export interface SessionCreatingProps extends LocalizableProps {
+	readonly state: Creating;
+}
+
+export function SessionCreating(props: SessionCreatingProps): JSX.Element {
+	const l = getLocalization(props, locales);
+
+	return (
+		<div className="SessionCreating">
+			{props.state.retries === 0 ? (
+				<div className="first">
+					<p>{l.loadingSession}</p>
+				</div>
+			) : (
+				<div className="failed">
+					<p>{l.failedLoadingSession}</p>
+				</div>
+			)}
+		</div>
+	);
+}
 
 export interface SessionLoadingProps extends LocalizableProps {
 	readonly state: Loading;
@@ -27,8 +49,15 @@ export function SessionLoading(props: SessionLoadingProps): JSX.Element {
 	);
 }
 
-const locales: Locales<SimpleString<"loadingSession" | "failedLoadingSession"> & { newSession: () => JSX.Element }> = {
+const locales: Locales<
+	SimpleString<"creatingSession" | "failedCreatingSession" | "loadingSession" | "failedLoadingSession"> & {
+		newSession: () => JSX.Element;
+	}
+> = {
 	en: {
+		creatingSession: "Creating session...",
+		failedCreatingSession: "Failed to create session. Trying again...",
+
 		loadingSession: "Loading session...",
 		failedLoadingSession: "Failed to load session. Trying again...",
 		newSession: () => (
@@ -38,6 +67,9 @@ const locales: Locales<SimpleString<"loadingSession" | "failedLoadingSession"> &
 		),
 	},
 	de: {
+		creatingSession: "Sitzung wird erstellt...",
+		failedCreatingSession: "Sitzung konnte nicht erstellt werden. Es wird erneut versucht...",
+
 		loadingSession: "Sitzung wird geladed...",
 		failedLoadingSession: "Sitzung konnte nicht geladen werden. Es wird erneut versucht...",
 		newSession: () => (
