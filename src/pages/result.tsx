5369789d21b02d0f4aa8c89b293a7dfce8bf77ef
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-03-02 21:07:46 +0100

    Slight progress for result page

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Page } from "../elements/page";
+import { SessionState } from "../elements/session-creating-loading";
 import { SharedHead } from "../elements/shared-header";
 import { getCurrentLang, getLocalization, Locales, LocalizableProps, SimpleString } from "../lib/localization";
 import { dynamic } from "../lib/react-util";
-import "./results.scss";
+import { Ready, useLoadSession } from "../lib/use-session";
+import "./result.scss";
 
 export default function ResultPage(): JSX.Element {
 	return (
@@ -23,9 +25,19 @@ export default function ResultPage(): JSX.Element {
 function Result(props: LocalizableProps): JSX.Element {
 	const l = getLocalization(props, locales);
 
+	const [state] = useLoadSession();
+
+	const onReady = ({ session }: Ready): JSX.Element => {
+		return (
+			<>
+				<p>TODO</p>
+			</>
+		);
+	};
+
 	return (
 		<Page {...props} className="Result" header="small">
-			TODO
+			<SessionState {...props} state={state} onReady={onReady} />
 		</Page>
 	);
 }
