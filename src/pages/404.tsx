a82f43fd15d2cc99ae908e463485f2eefa98ca1f
RunDevelopment <mitchi5000.ms@googlemail.com>
2022-05-25 13:13:53 +0200

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Page } from "../elements/page";
+import { SharedHead } from "../elements/shared-header";
+import { Locales } from "../lib/localization";
+import { useLocalization } from "../lib/use-localization";
+import { dynamic } from "../lib/react-util";
+import "./404.scss";
+
+export default function Error404Page(): JSX.Element {
+	return (
+		<>
+			{dynamic(() => (
+				<Error404 />
+			))}
+			<SharedHead />
+			<Helmet>
+				<title>Picapica - 404</title>
+			</Helmet>
+		</>
+	);
+}
+
+function Error404(): JSX.Element {
+	const l = useLocalization(locales);
+
+	return (
+		<Page className="Error404" header="small">
+			<h2 id="publisher">{l.title}</h2>
+			{l.content()}
+		</Page>
+	);
+}
+
+const locales: Locales<{
+	title: string;
+	content: () => JSX.Element;
+}> = {
+	en: {
+		title: "404 - Page not found",
+		content: () => (
+			<p>The page you requested could not be found. Please check that there are no typos in the URL.</p>
+		),
+	},
+	de: {
+		title: "404 - Seite nicht gefunden",
+		content: () => (
+			<p>
+				Die von Ihnen angefragte Seite konnte nicht gefunden werden. Bitte überprüfen Sie, dass der URL richtig
+				eingegeben wurde.
+			</p>
+		),
+	},
+};
